6bb38a7c72e3acd7a843ac53bb38e3cbe964c523
zhoupeng <zhoupeng.levi@bytedance.com>
2025-09-17 20:13:19 +0800

    feat: 悬浮工具栏新增提问按钮,点击后弹出modal框

--- app/components/text-selection-toolbar.tsx
+++ app/components/text-selection-toolbar.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useRef, useState } from "react";
+import { Modal, Input } from "antd";
 import styles from "./text-selection-toolbar.module.scss";
 import { IconButton } from "./button";
 import CopyIcon from "../icons/copy.svg";
 import PromptIcon from "../icons/prompt.svg";
 import EditIcon from "../icons/rename.svg";
 import { copyToClipboard } from "../utils";
+import Locale from "../locales";
 
 interface TextSelectionToolbarProps {
   range: Range;
@@ -21,6 +23,8 @@ export function TextSelectionToolbar({
   onAsk, // onHighlight,
 }: TextSelectionToolbarProps) {
   const [position, setPosition] = useState({ top: 0, left: 0 });
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [askInput, setAskInput] = useState("");
   const toolbarRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -71,16 +75,58 @@ export function TextSelectionToolbar({
     onClose();
   };
 
+  const handleAskModal = () => {
+    setAskInput(text);
+    setIsModalVisible(true);
+  };
+
+  const handleModalSend = () => {
+    onAsk(askInput);
+    setIsModalVisible(false);
+  };
+
+  const handleModalCancel = () => {
+    setIsModalVisible(false);
+  };
+
   return (
-    <div
-      className={styles["text-selection-toolbar"]}
-      ref={toolbarRef}
-      style={{ top: position.top, left: position.left }}
-      onMouseDown={(e) => e.preventDefault()}
-    >
-      <IconButton icon={<CopyIcon />} onClick={handleCopy} />
-      <IconButton icon={<PromptIcon />} onClick={handleAsk} />
-      <IconButton icon={<EditIcon />} onClick={handleHighlight} />
-    </div>
+    <>
+      <div
+        className={styles["text-selection-toolbar"]}
+        ref={toolbarRef}
+        style={{ top: position.top, left: position.left }}
+        onMouseDown={(e) => e.preventDefault()}
+      >
+        <IconButton
+          icon={<PromptIcon />}
+          onClick={handleAskModal}
+          // text={Locale.Chat.Ask}
+        />
+        <IconButton
+          icon={<CopyIcon />}
+          onClick={handleCopy}
+          // text={Locale.Chat.Copy}
+        />
+        <IconButton
+          icon={<EditIcon />}
+          onClick={handleHighlight}
+          // text={Locale.Chat.Highlight}
+        />
+      </div>
+      <Modal
+        title={Locale.Chat.AskQuestion}
+        open={isModalVisible}
+        onOk={handleModalSend}
+        onCancel={handleModalCancel}
+        destroyOnClose
+      >
+        <Input.TextArea
+          value={askInput}
+          onChange={(e) => setAskInput(e.target.value)}
+          rows={4}
+          placeholder={Locale.Chat.InputPlaceholder}
+        />
+      </Modal>
+    </>
   );
 }
